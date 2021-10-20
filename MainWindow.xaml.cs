0438ddda14d41babc4e6789afc99d37dbc20551a
Bradly valadez <d00297297@dmail.dixie.edu>
2021-10-20 13:01:33 -0600

    fixed repo to reflect local repo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Threading;
 using Microsoft.Win32;
 
 namespace WpfTutorialSamples.Audio_and_Video
 {
-	public partial class MediaPlayerAudioControlSample : Window
+	public partial class AudioVideoPlayerCompleteSample : Window
 	{
-		private MediaPlayer mediaPlayer = new MediaPlayer();
+		private bool mediaPlayerIsPlaying = false;
+		private bool userIsDraggingSlider = false;
 
-		public MediaPlayerAudioControlSample()
+		public AudioVideoPlayerCompleteSample()
 		{
 			InitializeComponent();
-
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
-			if (openFileDialog.ShowDialog() == true)
-				mediaPlayer.Open(new Uri(openFileDialog.FileName));
 
 			DispatcherTimer timer = new DispatcherTimer();
 			timer.Interval = TimeSpan.FromSeconds(1);
@@ -25,27 +22,81 @@ namespace WpfTutorialSamples.Audio_and_Video
 			timer.Start();
 		}
 
-		void timer_Tick(object sender, EventArgs e)
+		private void timer_Tick(object sender, EventArgs e)
 		{
-			if (mediaPlayer.Source != null)
-				lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
-			else
-				lblStatus.Content = "No file selected...";
+			if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+			{
+				sliProgress.Minimum = 0;
+				sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+				sliProgress.Value = mePlayer.Position.TotalSeconds;
+			}
 		}
 
-		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
 		{
-			mediaPlayer.Play();
+			e.CanExecute = true;
 		}
 
-		private void btnPause_Click(object sender, RoutedEventArgs e)
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
 		{
-			mediaPlayer.Pause();
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*";
+			if (openFileDialog.ShowDialog() == true)
+				mePlayer.Source = new Uri(openFileDialog.FileName);
 		}
 
-		private void btnStop_Click(object sender, RoutedEventArgs e)
+		private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
 		{
-			mediaPlayer.Stop();
+			e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
 		}
+
+		private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			mePlayer.Play();
+			mediaPlayerIsPlaying = true;
+		}
+
+		private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = mediaPlayerIsPlaying;
+		}
+
+		private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			mePlayer.Pause();
+		}
+
+		private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = mediaPlayerIsPlaying;
+		}
+
+		private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			mePlayer.Stop();
+			mediaPlayerIsPlaying = false;
+		}
+
+		private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+		{
+			userIsDraggingSlider = true;
+		}
+
+		private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+		{
+			userIsDraggingSlider = false;
+			mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+		}
+
+		private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+		}
+
+		private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+		}
+
 	}
 }
