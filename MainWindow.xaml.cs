ed9184a6743588a6fd397b39dba95e8497de70fe
Bradly valadez <d00297297@dmail.dixie.edu>
2021-10-22 02:04:44 -0600

    complete redo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,102 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Windows;
-using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using Microsoft.Win32;
 
-namespace WpfTutorialSamples.Audio_and_Video
+namespace MediaElementDemo
 {
-	public partial class AudioVideoPlayerCompleteSample : Window
-	{
-		private bool mediaPlayerIsPlaying = false;
-		private bool userIsDraggingSlider = false;
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		public AudioVideoPlayerCompleteSample()
-		{
-			InitializeComponent();
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Choose Media";
+            if (dialog.ShowDialog() == true)
+            {
+                Media.Source = new Uri(dialog.FileName);
+                MediaName.Text = dialog.FileName;
+            }
+        }
 
-			DispatcherTimer timer = new DispatcherTimer();
-			timer.Interval = TimeSpan.FromSeconds(1);
-			timer.Tick += timer_Tick;
-			timer.Start();
-		}
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.Source != null)
+                Media.Play();
+        }
 
-		private void timer_Tick(object sender, EventArgs e)
-		{
-			if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
-			{
-				sliProgress.Minimum = 0;
-				sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
-				sliProgress.Value = mePlayer.Position.TotalSeconds;
-			}
-		}
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.CanPause)
+                Media.Pause();
+        }
 
-		private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = true;
-		}
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.Source != null)
+                Media.Stop();
+        }
 
-		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*";
-			if (openFileDialog.ShowDialog() == true)
-				mePlayer.Source = new Uri(openFileDialog.FileName);
-		}
+        private void MuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Media.IsMuted = !Media.IsMuted;
+        }
 
-		private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
-		}
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.Volume = VolumeSlider.Value;
+        }
 
-		private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Play();
-			mediaPlayerIsPlaying = true;
-		}
+        private void Balance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.Balance = BalanceSlider.Value;
+        }
 
-		private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = mediaPlayerIsPlaying;
-		}
-
-		private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Pause();
-		}
-
-		private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-		{
-			e.CanExecute = mediaPlayerIsPlaying;
-		}
-
-		private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			mePlayer.Stop();
-			mediaPlayerIsPlaying = false;
-		}
-
-		private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
-		{
-			userIsDraggingSlider = true;
-		}
-
-		private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
-		{
-			userIsDraggingSlider = false;
-			mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
-		}
-
-		private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-			lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
-		}
-
-		private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
-		{
-			mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
-		}
-
-	}
+        private void Speed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.SpeedRatio = SpeedSlider.Value;
+        }
+    }
 }
